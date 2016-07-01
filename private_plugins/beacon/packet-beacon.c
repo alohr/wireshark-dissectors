26981102604d5863c21c6141154dba3fa660746c
Andreas Lohr <andr3as.lohr@gmail.com>
2016-07-01 15:34:16 +0200

    fixed compile errors

--- private_plugins/beacon/packet-beacon.c
+++ private_plugins/beacon/packet-beacon.c
@@ -10,11 +10,14 @@ static range_t *global_beacon_udp_range = NULL;
 static range_t *beacon_udp_range = NULL;
 
 static int proto_beacon = -1;
-
 static int hf_beacon_seqnum = -1;
 static int hf_beacon_timestamp = -1;
 
+static gint ett_beacon = -1;
+
 static dissector_handle_t beacon_handle;
+
+void proto_reg_handoff_beacon(void);
 
 
 static void nstimestamp_to_nstime(nstime_t *nstime, guint64 nstimestamp)
@@ -26,20 +29,18 @@ static void nstimestamp_to_nstime(nstime_t *nstime, guint64 nstimestamp)
 
 static int dissect_beacon(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void* data _U_)
 {
-    guint32 seqnum = tvb_get_letohl(tvb, 8);
-    guint64 t = tvb_get_letoh64(tvb, 24);
-    nstime_t transacttime = {};
+    guint32 seqnum = tvb_get_letohl(tvb, 0);
+    guint64 t = tvb_get_letoh64(tvb, 4);
+    nstime_t nstimestamp = {};
 
-    nstimestamp_to_nstime(&transacttime, t);
+    nstimestamp_to_nstime(&nstimestamp, t);
 
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "BEACON");
     col_clear(pinfo->cinfo, COL_INFO);
     
     col_add_fstr(pinfo->cinfo, COL_INFO, "SeqNum %u, Timestamp %s",
 		 seqnum,
-    		 abs_time_to_str(wmem_packet_scope(), &transacttime, ABSOLUTE_TIME_LOCAL, 0));
-
-    col_set_fence(pinfo->cinfo, COL_INFO);
+    		 abs_time_to_str(wmem_packet_scope(), &nstimestamp, ABSOLUTE_TIME_LOCAL, 0));
 
     if (tree) {
         proto_item *ti = NULL;
@@ -49,10 +50,10 @@ static int dissect_beacon(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U
         ti = proto_tree_add_item(tree, proto_beacon, tvb, 0, -1, ENC_NA);
         beacon_tree = proto_item_add_subtree(ti, ett_beacon);
 
-        proto_tree_add_item(tree, hf_beacon_seqnum, tvb, offset, 4, ENC_LITTLE_ENDIAN);
+        proto_tree_add_item(beacon_tree, hf_beacon_seqnum, tvb, offset, 4, ENC_LITTLE_ENDIAN);
         offset += 4;
 
-        proto_tree_add_item(tree, hf_beacon_seqnum, tvb, offset, 8, ENC_LITTLE_ENDIAN);
+        proto_tree_add_item(beacon_tree, hf_beacon_timestamp, tvb, offset, 8, ENC_LITTLE_ENDIAN);
         offset += 8;
     }
 
@@ -73,11 +74,11 @@ void proto_register_beacon(void)
 
     static hf_register_info hf[] = {
         {
-            &hf_beacon_seq,
+            &hf_beacon_seqnum,
             { "SequenceNumber", "beacon.SequenceNumber", FT_UINT32, BASE_DEC, NULL, 0x0, "", HFILL }
         },
         {
-            &hf_beacon_requesttime,
+            &hf_beacon_timestamp,
             { "Timestamp", "beacon.Timestamp", FT_ABSOLUTE_TIME, ABSOLUTE_TIME_LOCAL, NULL, 0x0, "", HFILL }
         },
     };
